d3ae435cdebeb78d3e2bc987b89ecfac7a97131c
William Eddie Plange <Wilplanh@gmail.com>
2025-07-12 10:34:43 +0000

    Updated python task

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,6 @@
+Length = 10
+Width = 5
+Area = Length * Width
+
+print("The area of the rectangle is:", Area)
+# rectangle_area.py
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000
+rate_of_interest = '0.05'  # 5% as a decimal
+time = 3
+simple_interest = (principal * float(rate_of_interest) * time)
+
+print("The Simple Interest is:", simple_interest)
+# simple_interest.py
