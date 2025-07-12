7e06ae84c1a3e7ec4a3e4028e2c2c85c6fcb51b5
William Eddie Plange <Wilplanh@gmail.com>
2025-07-12 11:30:34 +0000

    New python task

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+principal = 1000 # representing $1000
+rate = 0.05  # representing 5% annual interest rate
+time = 3 # representing 3 years
+# Calculate Simple Interest
+simple_interest = (principal * rate * time)
+
+print("The Simple Interest is:", simple_interest)
+# simple_interest.py
